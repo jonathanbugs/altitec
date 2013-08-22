3eca1710c02ef991cb3b2f430a6aed5db2bda51c
Jonathan Bugs <jonathan@cssroots.com.br>
2013-08-22 17:20:10 -0300

    institucional

---
+++ institucional.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Altitec</title>
+		<meta name="description" content="Sistema de Segurança em Altura">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<meta property="og:url" content="http://altitec.com.br"/>
+		<meta property="og:title" content="Altitec - Sistema de Segurança em Altura"/>
+		<meta property="og:image" content="http://altitec.com.br/img/facebook-thumb.jpg"/>
+		<meta property="og:site_name" content="Altitec"/>
+
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+		<link rel="shortcut icon" href="http://altitec.com.br/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="http://altitec.com.br/apple-touch-icon.png"/>
+
+
+		<link rel="stylesheet" href="css/reset.css"/>
+		<link rel="stylesheet" href="css/main.css"/>
+		<link rel="stylesheet" href="css/institucional.css"/>
+		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
+
+		<header id="topo">
+			<?php include "includes/topo.php"; ?>
+		</header>
+
+		<section id="conteudoPrincipal">
+			<div class="container clearfix">
+				<div id="main">
+					<div class="clearfix headerSessao">
+						<h2 class="tituloSessao">Institucional</h2>
+						<ul class="migalhaPao">
+							<li><a href="javascript:;">principal</a></li>
+							<li class="last"><a href="javascript:;">institucional</a></li>
+						</ul>
+					</div>
+					<div id="sessaoConteudo">
+						<div class="cms">
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sodales arcu sit amet elit eleifend, porttitor interdum neque dictum. Ut posuere porta ligula, varius mattis nibh malesuada vel. Cras vel leo suscipit est eleifend posuere. Donec vel pulvinar felis, non molestie quam. Ut nec facilisis leo. Sed ut gravida ipsum. Suspendisse potenti. Donec eu feugiat nibh. Nunc vitae dictum lacus. Nam tempor ipsum id risus aliquet, in convallis tortor tristique.
+							</p>
+						</div>
+
+						<ul id="institucionalUl" class="clearfix">
+							<li class="institucionalLi">
+								<img class="institucionalImg" src="img/novidades/novidade.jpg" alt="" />
+							</li>
+							<li class="institucionalLi">
+								<img class="institucionalImg" src="img/novidades/novidade.jpg" alt="" />
+							</li>
+							<li class="institucionalLi institucionalLiLast">
+								<img class="institucionalImg" src="img/novidades/novidade.jpg" alt="" />
+							</li>
+						</ul>
+					</div>
+
+					<a id="linkTopo" class="ir" href="#topo">Voltar ao Topo</a>
+				</div>
+				<aside id="sidebar">
+					<img src="img/side_produtos.jpg" alt="">
+					<span class="ttProdutosSide">Produtos</span>
+				</aside>	
+			</div>
+		</section>
+
+		<footer>
+			<?php include "includes/rodape.php"; ?>
+		</footer>
+
+		
+		<script src="js/vendor/jquery-1.9.1.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+
+		
+		<!-- Google Analytics -->
+		<script>
+			var _gaq=[['_setAccount','UA-42106713-1'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src='//www.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+	</body>
+</html>
