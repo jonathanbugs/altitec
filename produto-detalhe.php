0d9f83d52f326934d3cf34f341b77a68212fac29
Giuliano Varriale <giuliano.oca@gmail.com>
2013-08-22 15:00:48 -0300

    ;lk;lk

---
+++ produto-detalhe.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Altitec</title>
+		<meta name="description" content="Sistema de Segurança em Altura">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<meta property="og:url" content="http://altitec.com.br"/>
+		<meta property="og:title" content="Altitec - Sistema de Segurança em Altura"/>
+		<meta property="og:image" content="http://altitec.com.br/img/facebook-thumb.jpg"/>
+		<meta property="og:site_name" content="Altitec"/>
+
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+		<link rel="shortcut icon" href="http://altitec.com.br/favicon.ico" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="http://altitec.com.br/apple-touch-icon.png"/>
+
+
+		<link rel="stylesheet" href="css/reset.css"/>
+		<link rel="stylesheet" href="css/main.css"/>
+		<link rel="stylesheet" href="css/produto-detalhe.css"/>
+		<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
+
+		<header id="topo">
+			<?php include "includes/topo.php"; ?>
+		</header>
+
+		<section id="conteudoPrincipal">
+			<div id="main">
+				<div class="clearfix">
+					<h2>Descensor ID S Petzl</h2>
+					<ul class="migalhaPao">
+						<li><a href="javascript:;">principal</a></li>
+						<li><a href="javascript:;">produtos</a></li>
+						<li><a href="javascript:;">nome categoria</a></li>
+						<li><a href="javascript:;">Descensor ID S Petzl</a></li>
+					</ul>
+				
+					<span class="ttCategoria">Nome Categoria</span>
+				</div>
+				<div id="sobreProduto">
+					<div class="cms">
+						<img src="../img/produto-detalhe.jpg" alt="">
+
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sodales arcu sit amet elit eleifend, porttitor interdum neque dictum. Ut posuere porta ligula, varius mattis nibh malesuada vel. Cras vel leo suscipit est eleifend posuere. Donec vel pulvinar felis, non molestie quam. Ut nec facilisis leo. Sed ut gravida ipsum. Suspendisse potenti. Donec eu feugiat nibh. Nunc vitae dictum lacus. Nam tempor ipsum id risus aliquet, in convallis tortor tristique.</p>
+						<p>Integer lobortis ligula et dui porttitor commodo. Proin viverra magna in augue hendrerit laoreet. Fusce ut mattis erat. Pellentesque sed tortor erat. Fusce commodo fringilla porta. Nam lorem diam, rhoncus eget blandit nec, semper cursus leo. Mauris a justo a ligula vestibulum mollis placerat vel eros. Integer massa enim, laoreet id velit ut, dictum blandit purus. Phasellus non rutrum dolor, et vestibulum turpis. Mauris ut vestibulum massa. Morbi a erat sem. Sed libero orci, euismod non tincidunt consequat, bibendum et magna. Suspendisse potenti. Nam aliquet, eros a mattis pellentesque, ante nulla pharetra justo, eget dapibus eros purus vel sem.</p>
+					</div>
+
+					<a href="javascript:;" id="btRealizarorcamento">(Realizar orçamento)</a>
+
+					<form action="javascript:;" id="formOrcamento">
+						<ul>
+							<li>
+								<label for="iptNome">Nome:</label>
+								<input type="text" id="iptNome" name="iptNome">
+							</li>
+							<li>
+								<label for="iptEmail">E-mail:</label>
+								<input type="text" id="iptEmail" name="iptEmail">
+							</li>
+							<li>
+								<label for="iptFone">Fone:</label>
+								<input type="text" id="iptFone" name="iptFone">
+
+								<label for="IptCidade">Cidade/UF:</label>
+								<input type="text" id="iptCidade" name="iptCidade">
+							</li>
+							<li>
+								<label for="txtAreaMensagem">Mensagem:</label>
+								<textarea name="txtAreaMensagem" id="txtAreaMensagem" cols="30" rows="10"></textarea>
+							</li>
+							<li>
+								<button id="btEnviarOrcarmento">Enviar</button>
+							</li>
+						</ul>
+					</form>	
+
+					<div class="voltarTopo">
+						<a id="linkTopo" class="ir" href="#topo">Voltar ao Topo</a>
+					</div>
+				</div>
+			</div>
+
+			<aside id="sidebar">
+				<img src="../img/sideProdutos.jpg" alt="">
+
+				<span class="ttProdutosSide">Produtos</span>
+
+				<nav id="navLocal">
+					<ul>
+						<li>
+							<a href="javascript:;">Nome Categoria</a>
+						</li>
+						<li>
+							<a href="javascript:;">Nome Categoria</a>
+						</li>
+						<li>
+							<a href="javascript:;">Nome Categoria</a>
+						</li>
+						<li>
+							<a href="javascript:;">Nome Categoria</a>
+						</li>
+						<li>
+							<a href="javascript:;">Nome Categoria</a>
+						</li>
+						<li>
+							<a href="javascript:;">Nome Categoria</a>
+							<ul>
+								<li><a href="javascript:;">Nome Categoria</a></li>
+								<li><a href="javascript:;">Nome Categoria</a></li>
+								<li><a href="javascript:;">Nome Categoria</a></li>
+							</ul>
+						</li>
+					</ul>
+				</nav>	
+			</aside>
+		</section>
+
+		<footer>
+			<?php include "includes/rodape.php"; ?>
+		</footer>
+
+		
+		<script src="js/vendor/jquery-1.9.1.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+
+		
+		<!-- Google Analytics -->
+		<script>
+			var _gaq=[['_setAccount','UA-42106713-1'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src='//www.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+	</body>
+</html>
